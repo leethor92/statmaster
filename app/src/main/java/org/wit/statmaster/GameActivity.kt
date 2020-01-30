c989aa9814e28cc4191c0e51cbd17092d21c03fa
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2020-01-30 22:02:18 +0000

    early changes to file names & introduction of ankologger for info purposes

--- app/src/main/java/org/wit/statmaster/GameActivity.kt
+++ app/src/main/java/org/wit/statmaster/GameActivity.kt
@@ -2,11 +2,14 @@ package org.wit.statmaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() , AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
+        info("Game Activity started..")
     }
 }
